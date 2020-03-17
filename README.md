e10407d1c8bc123f5cee46528129fcceffa8890f
Aytekin Yaliz <aytekinyaliz@gmail.com>
2020-03-17 01:08:13 +0000

    initials

---
+++ README.md
@@ -0,0 +1,1 @@
+# YLZ-XYZ-AUTH-MDW
---
+++ index.js
@@ -0,0 +1,17 @@
+const { HttpStatusCode } = require('./src/libs/constants');
+
+const { errorHandler } = require('./src/middlewares/errorHandler');
+const { pageNotFoundHandler } = require('./src/middlewares/pageNotFoundHandler');
+
+
+module.exports = {
+  libs: {
+    constants: {
+      HttpStatusCode
+    }
+  },
+  middlewares: {
+    errorHandler,
+    pageNotFoundHandler
+  }
+}
